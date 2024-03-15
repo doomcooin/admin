7feea9c336e363351afd80b3acf970c43ff4f18f
Nik Nike <doomcooin@gmail.com>
2024-03-15 12:23:53 +0200

    test

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 import { config } from "dotenv";
  config();
 import TelegramBot from "node-telegram-bot-api";
-//const botToken = new Bot(process.env.BOT_API_KEY)
-//const testKey = "6469a721bede21a8ed89";
-//import stripe from "stripe";
-//import axios from "axios";
 
-//const botToken = "6985342414:AAFxpkOhbpnMLBgpI_j9AX_jrHIuNwQrmug";
 
 //const chatId = "1227459883";
 const webAppUrl = "https://biznewschannel.com/";
